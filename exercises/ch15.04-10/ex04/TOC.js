b412b1509175c0be8b7b5bdbfb4c8bb06f5a59ac
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-08-23 11:29:23 +0900

    Update TOC.js

--- exercises/ch15.04-10/ex04/TOC.js
+++ exercises/ch15.04-10/ex04/TOC.js
@@ -1,5 +1,3 @@
-/* ブックマークレットとしてブラウザに登録する場合は以下のコードの先頭に javascript: を追加 */
-/* // から始まるコメントはブックマークレット内で正しく動作しないことがあるため注意 */
 (() => {
   /* NOTE: ブックマークレットによって実行する想定のため DOMContentLoaded のイベントで実行しない */
   let toc = document.querySelector("#TOC");
@@ -17,30 +15,30 @@
     document.body.prepend(style);
   }
   style.textContent = `
-  #TOC {
-    border: solid black 1px;
-    margin: 10px;
-    padding: 10px;
-
-    /* 追加 (右上に固定表示) */
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    width: 250px;
-    padding: 10px;
-    background-color: #f0f0f0;
-    border: 1px solid #ccc;
-    overflow-y: auto;
-    max-height: calc(100vh - 40px);
-    z-index: 9999;
-  }
-  .TOCEntry { margin: 5px 0px; }
-  .TOCEntry a { text-decoration: none; }
-  .TOCLevel1 { font-weight: bold; }
-  .TOCLevel2 { margin-left: .25in; }
-  .TOCLevel3 { margin-left: .5in; }
-  .TOCSectNum:after { content: ": "; }
-      `;
+    #TOC {
+      border: solid black 1px;
+      margin: 10px;
+      padding: 10px;
+  
+      /* 追加 (右上に固定表示) */
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      width: 250px;
+      padding: 10px;
+      background-color: #f0f0f0;
+      border: 1px solid #ccc;
+      overflow-y: auto;
+      max-height: calc(100vh - 40px);
+      z-index: 9999;
+    }
+    .TOCEntry { margin: 5px 0px; }
+    .TOCEntry a { text-decoration: none; }
+    .TOCLevel1 { font-weight: bold; }
+    .TOCLevel2 { margin-left: .25in; }
+    .TOCLevel3 { margin-left: .5in; }
+    .TOCSectNum:after { content: ": "; }
+        `;
 
   /* NOTE: 以下は書籍のサンプルと同じ */
   let headings = document.querySelectorAll("h2,h3,h4,h5,h6");
@@ -54,7 +52,7 @@
     for (let i = level; i < sectionNumbers.length; i++) {
       sectionNumbers[i] = 0;
     }
-    let sectionNumber = sectionNumbers.slice(0, level).join(".");
+    let sectionNumber = sectionNumbers.slice(0, level).join("-");
     let span = document.createElement("span");
     span.className = "TOCSectNum";
     span.textContent = sectionNumber;
@@ -69,6 +67,14 @@
     let link = document.createElement("a");
     link.href = `#${fragmentName}`;
     link.innerHTML = heading.innerHTML;
+
+    /* 追加: スムーズスクロール */
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      document
+        .querySelector(`a[name='${fragmentName}']`)
+        .scrollIntoView({ behavior: "smooth" });
+    });
 
     let entry = document.createElement("div");
     entry.classList.add("TOCEntry", `TOCLevel${level}`);
