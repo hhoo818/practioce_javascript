f426700ffcbb2a9d120b0275022e3d189bb86453
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-10-16 11:37:42 +0900

    e

--- exercises/ch16/ex13/shell.js
+++ exercises/ch16/ex13/shell.js
@@ -69,6 +69,9 @@ async function runcmd(cmd, stdin = null, stdout = null) {
       {
         // FIXME: ここを実装してね (2行程度)
         // HINT: cmd.file のストリームを createWriteStream で作成し runcmd を再帰的に呼び出す
+        const writeStream = fs.createWriteStream(cmd.file); // ファイルストリームを作成
+        await runcmd(cmd.cmd, stdin, writeStream); // 再帰的に runcmd を実行し出力をファイルにリダイレクト
+        writeStream.end(); // ストリームを終了
       }
       break;
 
@@ -76,6 +79,8 @@ async function runcmd(cmd, stdin = null, stdout = null) {
       {
         // FIXME: ここを実装してね (2行程度)
         // HINT: cmd.file のストリームを createReadStream で作成し runcmd を再帰的に呼び出す
+        const readStream = fs.createReadStream(cmd.file);
+        await runcmd(cmd.cmd, readStream, stdout);
       }
       break;
 
@@ -84,6 +89,12 @@ async function runcmd(cmd, stdin = null, stdout = null) {
         // FIXME: ここを実装してね (4行程度)
         // HINT: cmd.left と cmd.right に対して runcmd を再帰的に呼び出し Promise.all で待つ
         // HINT: left と right を繋ぐには new PassThrought() で作成したストリームを使用する
+        const passThrough = new PassThrough();
+        await Promise.all([
+          runcmd(cmd.left, stdin, passThrough),
+          runcmd(cmd.right, passThrough, stdout),
+        ]);
+        passThrough.end();
       }
       break;
 
