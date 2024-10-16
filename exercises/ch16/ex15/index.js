5696e28b3adb485b4936aaa0a7102599f822f069
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-10-16 17:39:22 +0900

    ss

---
+++ exercises/ch16/ex15/index.js
@@ -0,0 +1,33 @@
+import { isMainThread, Worker, parentPort } from "worker_threads";
+
+if (isMainThread) {
+  let num = 0;
+  let worker = new Worker(__filename);
+
+  worker.on("online", () => {
+    console.log("Worker is online");
+  });
+
+  worker.on("message", (message) => {
+    if (message === "num をインクリメントせよ") {
+      num++;
+    } else if (message === "done") {
+      console.log(num);
+    }
+  });
+
+  for (let i = 0; i < 10_000_000; i++) {
+    num++;
+  }
+
+  worker.postMessage("start");
+} else {
+  parentPort.on("message", (message) => {
+    if (message === "start") {
+      for (let i = 0; i < 10_000_000; i++) {
+        parentPort.postMessage("num をインクリメントせよ");
+      }
+      parentPort.postMessage("done");
+    }
+  });
+}
